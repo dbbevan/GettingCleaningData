1f196caf5bf0243e348a51dbbdb02b0457657a50
dbbevan <dbevan@outlook.com>
2014-07-26 17:09:03 -0700

    Added Project Source zip url for easy reference.
    https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip

--- run_analysis.R
+++ run_analysis.R
@@ -10,7 +10,7 @@
 #       3.Uses descriptive activity names to name the activities in the data set
 #       4.Appropriately labels the data set with descriptive variable names. 
 #       5.Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
-
+#      Get code from here https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
 # 1.Merges the training and the test sets to create one data set.
         # set my working directory
